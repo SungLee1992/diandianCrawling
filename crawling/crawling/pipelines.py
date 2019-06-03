a495de765434ed7ff30557e87423c4dcf3854caa
sunglee <sunglee@nwafu.edu.cn>
2019-06-03 18:43:13 +0800

    增加中国农产品网爬虫，增加mysql处理部分

--- crawling/crawling/pipelines.py
+++ crawling/crawling/pipelines.py
@@ -10,37 +10,145 @@
 # 2、一个spider的内容可能要做不同的操作，比如存入不同的数据库中
 
 import logging
+import pymysql.cursors
+from sqlalchemy import create_engine,Column,Integer,String,Table,MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import settings
 
 logger = logging.getLogger(__name__)
+
+
+class DB_Pipeline(object):
+    # def __init__(self):  # 执行爬虫时
+    #     self.engine = create_engine('mysql://root:0@localhost:3306/spider?charset=utf8', echo=True)  # 连接数据库
+    #     self.session = sessionmaker(bind=self.engine)
+    #     self.sess = self.session()
+    #     Base = declarative_base()
+    #     # 动态创建orm类,必须继承Base, 这个表名是固定的,如果需要为每个爬虫创建一个表,请使用process_item中的
+    #     self.Article = type('article_articletest', (Base, ArticleTemplate), {'__tablename__': 'article_article'})
+    #
+    # def process_item(self, item, spider):  # 爬取过程中执行的函数
+    #     # 按照爬虫名动态创建一个类
+    #     # if not hasattr(self,spider.name):
+    #     #     self.Article = type(spider.name, (Base, ArticleTemplate), {'__tablename__': spider.name, })
+    #     # 在数据库中创建这个表
+    #     # if spider.name not in self.engine.table_names(): #create table for this spider
+    #     #     self.Article.metadata.create_all(self.engine)
+    #
+    #     self.sess.add(self.Article(**item))
+    #     self.sess.commit()
+    #
+    # def close_spider(self, spider):  # 关闭爬虫时
+    #     self.sess.close()
+
+    def __init__(self):
+        # 连接数据库
+        self.connect = pymysql.connect(
+            host=settings.MYSQL_HOST,  # 数据库地址
+            port=settings.MYSQL_PORT,  # 数据库端口
+            db=settings.MYSQL_DBNAME,  # 数据库名
+            user=settings.MYSQL_USER,  # 数据库用户名
+            passwd=settings.MYSQL_PASSWD,  # 数据库密码
+            charset='utf8',  # 编码方式
+            use_unicode=True)
+        # 通过cursor执行增删查改
+        self.cursor = self.connect.cursor()
+
+    '''
+    重写process_item方法
+    '''
+
+    def process_item(self, item, spider):
+        supplyItems = []
+        purchaseItems = []
+        try:
+
+            if item["type"] == "supply":
+                # 查重处理
+                self.cursor.execute(
+                    """select * from supply where info_from = %s""",
+                    item['img_url'])
+                # 是否有重复数据
+                repetition = self.cursor.fetchone()
+
+                # 重复
+                if repetition:
+                    pass
+                else:
+                    sql = '''insert into supply(name, info, rating, num ,quote, img_url) value (%s, %s, %s, %s, %s, %s)'''
+                    supplyItems = supplyItems.append(item['name'], item['info'], item['rating'], item['num'], item['quote'],
+                                                 item['img_url'])
+                    self.cursor.execute(sql, supplyItems)
+
+            if item["type"] == "purchase":
+                # 查重处理
+                self.cursor.execute(
+                    """select * from supply where info_from = %s""",
+                    item['img_url'])
+                # 是否有重复数据
+                repetition = self.cursor.fetchone()
+
+                # 重复
+                if repetition:
+                    pass
+                else:
+                    sql = '''insert into purchase(name, info, rating, num ,quote, img_url) value (%s, %s, %s, %s, %s, %s)'''
+                    purchaseItems = purchaseItems.append(item["result_item"], item['info'], item['rating'], item['num'], item['quote'])
+                    self.cursor.execute(sql, supplyItems)
+
+            # 提交sql语句
+            self.connect.commit()
+
+        except Exception as error:
+            # 出现错误时打印错误日志
+            logger.error(error)
+        return item
+
+    def insert_into_table(self, conn, item):
+        conn.execute(
+            'insert into zreading(title,author,pub_date,types,tags,view_counts,content) values(%s,%s,%s,%s,%s,%s,%s)', (
+                item['title'], item['author'], item['pub_date'], item['types'], item['tags'], item['view_count'],
+                item['content']))
+
 
 # 陕西农业农村厅需求和供应数据处理
 class Sxnynct_SupAndPur_Pipeline(object):
     def process_item(self, item, spider):
         if spider.name == "Sxnynct_SupAndPur_Spider":
-            #供应信息处理
+            # 供应信息处理
             if item["type"] == "supply":
                 print(item)
-                print(item["result_item"]["pub_title"]+"-"*20)
+                print(item["result_item"]["pub_title"] + "-" * 20)
                 print(item["content"])
 
-            #需求信息处理
+            # 需求信息处理
             if item["type"] == "purchase":
                 print(item["type"] + "-" * 20)
-                print(item["result_item"]["pub_title"]+"-"*20)
+                print(item["result_item"]["pub_title"] + "-" * 20)
                 print(item["content"])
         return item
 
-# 中国农产品网需求和供应爬虫
+
+# 中国农产品网供应爬虫
 class Zgncpw_Pur_Pipeline(object):
     def process_item(self, item, spider):
         if spider.name == "Zgncpw_Pur_Spider":
-            # #供应信息处理
-            # if item["type"] == "supply":
-            #     print(item["type"]+"-"*20)
-            #     print(item["content"])
-
-            #需求信息处理
+            # 需求信息处理
             if item["type"] == "purchase":
-                print(item["type"] + "-" * 20+item["result_item"]["pub_title"])
-                print(item["result_item"]["pur_require"])
+                print(item["type"] + "-" * 20)
+                print(item["result_item"])
+        return item
+
+
+# 中国农产品网供应爬虫
+class Zgncpw_Sup_Pipeline(object):
+    def process_item(self, item, spider):
+        if spider.name == "Zgncpw_Sup_Spider":
+            # 供应信息处理
+            if item["type"] == "supply":
+                print(item["type"] + "-" * 20)
+                print(item["result_item"])
+
         return item
--- crawling/crawling/spiders/Zgncpw_Sup_Spider.py
+++ crawling/crawling/spiders/Zgncpw_Sup_Spider.py
@@ -8,10 +8,10 @@ from crawling.SupplyItem import SupplyItem
 logger = logging.getLogger(__name__)  # "__name"可以取到当前文件名Zgncpw_Pur_Spider.py
 
 
-class Zgncpw_Pur_Spider(scrapy.Spider):
-    name = 'Zgncpw_Pur_Spider'  # 爬虫名
+class Zgncpw_Sup_Spider(scrapy.Spider):
+    name = 'Zgncpw_Sup_Spider'  # 爬虫名
     allowed_domains = ['zgncpw.com']  # 允许爬的范围
-    start_urls = ['http://www.zgncpw.com/sell/']  # 最开始请求的url地址，直接爬取全部分类和全部地区
+    start_urls = ['http://www.zgncpw.com/sell/']
     # sup_listIds = ['13', '9', '16', '6', '202', '212', '17',
     #                '1123']  # 分别是生鲜水果、新鲜蔬菜、畜牧水产、米面粮油、农副产品、农资产品、花卉苗木、中草药材供应信息url的listId
 
@@ -21,78 +21,121 @@ class Zgncpw_Pur_Spider(scrapy.Spider):
     """
 
     def parse(self, response):
-        for listId in self.pur_listIds:
-            cur_category_url = self.start_urls[0] + "list-{}.html".format(listId)  # 大类的url
-            print(cur_category_url)
+        for listId in self.sup_listIds:
+            category_url = self.start_urls[0] + "list-{}.html".format(listId)  # 大类的url
+            print("大类地址："+category_url)
             # 在每个大类中按照物品名称进行小分类
             yield scrapy.Request(
-                cur_category_url,
+                category_url,
                 callback=self.parse_category,
                 meta={"type": "supply"}
             )
 
-    def parse_category(self, response):
+    """
+    小分类解析
+    """
+    def parse_category(self,response):
+        a_list = response.xpath("//div[@class='bg-gray clearfix']//a")  # 取a列表
+        for a in a_list:
+            item = SupplyItem()
+            item["pro_name"] = a.xpath("./text()").extract_first().strip()  # 取分类名
+            cur_category_url = a.xpath("./@href").extract_first()
+            yield scrapy.Request(
+                cur_category_url,
+                callback=self.parse_pro,
+                meta={"type": "supply", "item": item}
+            )
+
+    def parse_pro(self, response):
         items = []  # 存放SupplyItem集合
         li_list = response.xpath("//div[@class='list pad10 clearfix show-hide-data']//ul/li")  # 取ul列表
         # 解析li列表
+        item = response.meta["item"]
         for li in li_list:
-            item = SupplyItem()
-            item['pro_name'] = li.xpath(".a[@class='catname tit-bg-orange pos-abs']").extract_first()
-            item['pub_address'] = li.xpath("./span[2]/text()").extract_first()
-            item['pur_user'] = li.xpath("./span[3]/text()").extract_first()
-            item['pub_time'] = li.xpath("./span[4]/text()").extract_first()
-            item['info_from'] = li.xpath("./span[5]/a/@href").extract_first()
+            item['sup_variety'] = li.xpath("./a[@class='catname tit-bg-orange pos-abs']/text()").extract_first()
+            item['pro_price'] = li.xpath(".//a[@class='font-gray-5']//div[@class='pad-t-d-10 text-align-c clearfix']/span/text()").extract_first()
+            item['pub_title'] = li.xpath(".//a[@class='font-gray-5']//div[@class='pad-t-d-10 text-align-c clearfix']/h1/text()").extract_first()
+            item['sup_pics'] = li.xpath(".//a[@class='font-gray-5']//img/@src").extract_first()
+            item['info_from'] = li.xpath(".//a[@class='font-gray-5']/@href").extract_first()
 
             # url不为空，则请求详细页
-            # 二级详细页解析
+            # 详细页解析
             if item['info_from'] is not None:
                 yield scrapy.Request(
                     item['info_from'],
                     callback=self.parse_detail,  # 详细页的解析
-                    meta={"item": deepcopy(item), "type": "purchase"}
+                    meta={"item": deepcopy(item), "type": "supply"}
                 )
     #
-    #     # 翻页
-    #     page_count = 10  # 最多爬n页
-    #     cur_page = int(response.xpath("//div[@class='pages']/strong/text()").extract_first().strip())  # 取当前页页码
-    #     if cur_page in range(1, page_count):
-    #         next_page_url = response.xpath("//div[@class='pages']/a[last()]/@href").extract_first()  # 取“下一页”链接
-    #         print("下一页：" + next_page_url)
-    #         yield scrapy.Request(
-    #             next_page_url,
-    #             callback=self.parse_category,
-    #             meta={"item": item, "type": response.meta["type"]}
-    #         )
-    #
-    # """
-    # 供应信息详细页的解析
-    # """
-    #
-    # def parse_detail(self, response):
-    #     item = response.meta["item"]
-    #
-    #     # 爬详细
-    #     item["pur_num"] = response.xpath("//table[@cellpadding='5']//td[2]/text()").extract()[0]
-    #     price = response.xpath("//table[@cellpadding='5']//td[2]/text()").extract()[1]
-    #     try:
-    #         packages = response.xpath("//table[@cellpadding='5']//td[2]/text()").extract()[2]
-    #     except Exception as e:
-    #         packages = ""
-    #
-    #     try:
-    #         item["pur_address"] = response.xpath("//table[@cellpadding='5']//td[2]/text()").extract()[3]
-    #     except Exception as e:
-    #         item["pur_address"] = ""
-    #
-    #     try:
-    #         item["end_time"] = response.xpath("//table[@cellpadding='5']//td[2]/text()").extract()[4]
-    #     except Exception as e:
-    #         item["end_time"] = ""
-    #
-    #     # 拼接采购需求
-    #     item["pur_require"] = "需求数量" + item["pur_num"] + ",价格" + price + "," + packages + "," + item[
-    #         "pur_address"] + "," + item["end_time"]
-    #     # 构造传递给pipeline的数据结构
-    #     result_map = {"result_item": item, "type": response.meta["type"]}
-    #
-    #     yield result_map
+        # 翻页
+        page_count = 1  # 最多爬n页
+        cur_page_str = response.xpath("//div[@class='pages']/strong/text()").extract_first()
+        if cur_page_str is not None :
+            cur_page = int(cur_page_str.strip())  # 取当前页页码
+            if cur_page in range(1, page_count):
+                next_page_url = response.xpath("//div[@class='pages']/a[last()]/@href").extract_first()  # 取“下一页”链接
+                print("下一页：" + next_page_url)
+                yield scrapy.Request(
+                    next_page_url,
+                    callback=self.parse_pro,
+                    meta={"item": item, "type": response.meta["type"]}
+                )
+
+
+    """
+    供应信息详细页的解析
+    """
+
+    def parse_detail(self, response):
+        item = response.meta["item"]
+
+        # 爬详细
+        ul = response.xpath("//ul[@class='two l-big line-height-36 clearfix']")
+        try:
+            item["pro_price"] = ul.xpath("./li[1]/text()").extract_first()
+        except Exception as e:
+            item["pro_price"] = ""
+
+        try:
+            min_order_num = ul.xpath("./li[2]/text()").extract_first()
+        except Exception as e:
+            min_order_num = ""
+
+        try:
+            item["sup_num"] = ul.xpath("./li[3]/text()").extract_first()
+        except Exception as e:
+            item["sup_num"] = ""
+
+        try:
+            send_date = ul.xpath("./li[4]/span[2]/text()").extract_first()
+        except Exception as e:
+            send_date = ""
+
+        try:
+            item["pub_address"] = ul.xpath("./li[5]/text()").extract_first()
+        except Exception as e:
+            item["pub_address"] = ""
+
+        try:
+            item["end_time"] = ul.xpath("./li[6]/text()").extract_first()
+        except Exception as e:
+            item["end_time"] = ""
+
+        try:
+            item["pub_time"] = ul.xpath("./li[7]/text()").extract_first()
+        except Exception as e:
+            item["pub_time"] = ""
+
+        # 拼接采购需求
+        if min_order_num is not "" and min_order_num is not None :
+            item["sup_description"] = "起订："+ min_order_num+"。"
+        else :
+            item["sup_description"] = ""
+
+        if send_date is not  "" and send_date is not None:
+            item["sup_description"] = item["sup_description"]+"发货期限：自买家付款之日起"+send_date+"天内发货。"
+
+        # 构造传递给pipeline的数据结构
+        result_map = {"result_item": item, "type": response.meta["type"]}
+
+        yield result_map
