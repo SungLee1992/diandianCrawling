5685757785a8a4801fa54de4657cca7fd32ee83c
sunglee <sunglee_1992@163.com>
2019-07-13 00:29:53 +0800

    固原市价格信息，详细页解析（完成），下一步批量入库

--- crawling/crawling/GysPriceItem.py
+++ crawling/crawling/GysPriceItem.py
@@ -9,10 +9,11 @@ import scrapy
 
 class GysPriceItem(scrapy.Item):
     pro_name = scrapy.Field()  # 产品名称
+    category = scrapy.Field()  # 产品类别
     pro_unit = scrapy.Field()  # 产品单位
-    pro_price = scrapy.Field()  # 产品价格
+    yzq_price = scrapy.Field()  # 原州区产品价格
+    ldx_price = scrapy.Field()  # 隆德县产品价格
     avg_price = scrapy.Field()  #平均价格
-    address = scrapy.Field()  # 发布地区
     pub_time = scrapy.Field()  # 发布时间
     price_from = scrapy.Field()  # 信息来源
     pass
--- crawling/crawling/spiders/Gys_Price_Spider.py
+++ crawling/crawling/spiders/Gys_Price_Spider.py
@@ -21,10 +21,10 @@ class Gys_Price_Spider(scrapy.Spider):
 
     def parse(self, response):
         print("#"*20)
+        # now_year = datetime.datetime.now().year
         # now_month = datetime.datetime.now().month
         now_year = 2019
-        now_month = 5
-        #item = GysPriceItem()
+        now_month = 3
         ul_list = response.xpath("//div[@class='xxgk_wrap']//ul")
         for ul in ul_list:
             title = ul.xpath(".//li[@class='w383']/a/text()").extract_first()
@@ -32,8 +32,8 @@ class Gys_Price_Spider(scrapy.Spider):
             pub_month = datetime.datetime.strptime(pub_time, "%Y-%m-%d").month
             pub_year = datetime.datetime.strptime(pub_time, "%Y-%m-%d").year
             # 只爬当月数据
-            # if ("农畜产品" in title) and (pub_month == now_month) and (pub_year == now_year):
-            if "农畜产品" in title:
+            #if ("农畜产品" in title) and (pub_month == now_month) and (pub_year == now_year):
+            if ("农畜产品" in title) and (pub_year >= 2019):
                 price_info_url = "http://www.nxgy.gov.cn/zwgk/zfxxgkml/nygjcjgxgg/"+ul.xpath(".//li[@class='w383']/a/@href").extract_first().lstrip("./")
                 yield scrapy.Request(
                     price_info_url,
@@ -45,18 +45,17 @@ class Gys_Price_Spider(scrapy.Spider):
 
 
         # 翻页
-        # page_count = int(re.findall('createPageHTML\((.), ., "list", "html"\)', response.body.decode("UTF-8"))[0])
-        # print("page_count:"+str(page_count))
-        # cur_page = int(re.findall('createPageHTML\(., (.), "list", "html"\)', response.body.decode("UTF-8"))[0])
-        # print("cur_page:"+str(cur_page))
-        # if cur_page in range(0,page_count):
-        #     next_page = "http://www.nxgy.gov.cn/zwgk/zfxxgkml/nygjcjgxgg/list_{}.html".format(cur_page+1)
-        #     print(next_page)
-        #     yield scrapy.Request(
-        #         next_page,
-        #         callback=self.parse,
-        #         meta={"item": deepcopy(item)}
-        #     )
+        page_count = int(re.findall('createPageHTML\((.), ., "list", "html"\)', response.body.decode("UTF-8"))[0])
+        print("page_count:"+str(page_count))
+        cur_page = int(re.findall('createPageHTML\(., (.), "list", "html"\)', response.body.decode("UTF-8"))[0])
+        print("cur_page:"+str(cur_page))
+        if cur_page in range(0,page_count):
+            next_page = "http://www.nxgy.gov.cn/zwgk/zfxxgkml/nygjcjgxgg/list_{}.html".format(cur_page+1)
+            print(next_page)
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse,
+            )
 
     """
     价格信息详细页的解析
@@ -64,30 +63,42 @@ class Gys_Price_Spider(scrapy.Spider):
 
     def parse_detail(self, response):
         all_tr = response.xpath("//div[@class='ue_table']//tr")
+        item = GysPriceItem()
         if all_tr is not None:
-            for index in range(3, len(all_tr)):
+            items = []
+            for index in range(1, len(all_tr)):
                 # 第一组品种
                 # 处理品种名
-                # pro_name = all_tr[index].xpath(".//td[1]//span/text()").extract_first()
-                # if (pro_name is None) or ("品 种" in pro_name) or ("填" in pro_name):
-                #     continue
-                # pro_name = ''.join(pro_name.split())
-                # if "、" in pro_name:
-                #     category = pro_name.split("、")[1]
-                #     pro_name = pro_name.split("、")[1]
-                #
-                # # 处理原州区价格、隆德县价格、均价
-                # yzq_price = all_tr[index].xpath(".//td[3]//span/text()").extract_first()
-                # if yzq_price is None:
-                #     yzq_price = ""
-                # ldx_price = all_tr[index].xpath(".//td[4]//span/text()").extract_first()
-                # if ldx_price is None:
-                #     ldx_price = ""
-                # avg_price = all_tr[index].xpath(".//td[5]//span/text()").extract_first()
-                # if avg_price is None:
-                #     avg_price = ""
-                # print(pro_name+","+category+","+yzq_price+","+ldx_price+","+avg_price)
+                pro_name = all_tr[index].xpath(".//td[1]//span/text()").extract_first()
+                if (pro_name is None) or ("品 种" in pro_name) or ("填" in pro_name):
+                    continue
+                pro_name = ''.join(pro_name.split())        #去掉所有空格
+                if "、" in pro_name:
+                    category = pro_name.split("、")[1]
+                    pro_name = pro_name.split("、")[1]
+                # 处理原州区价格、隆德县价格、均价
+                yzq_price = all_tr[index].xpath(".//td[3]//span/text()").extract_first()
+                if yzq_price is None:
+                    yzq_price = ""
+                ldx_price = all_tr[index].xpath(".//td[4]//span/text()").extract_first()
+                if ldx_price is None:
+                    ldx_price = ""
+                avg_price = all_tr[index].xpath(".//td[5]//span/text()").extract_first()
+                if avg_price is None:
+                    avg_price = ""
 
+                item['pro_name'] = pro_name
+                item['category'] = category
+                item['pro_unit'] = "元/公斤"
+                item['yzq_price'] = yzq_price.strip()
+                item['ldx_price'] = ldx_price.strip()
+                item['avg_price'] = avg_price.strip()
+                item['pub_time'] = response.meta["pub_time"]
+                item['price_from'] = "固原市农业农村局"
+
+                items.append(deepcopy(item))
+
+            for index in range(1,len(all_tr)):
                 # 第二组品种
                 # 处理品种名
                 pro_name = all_tr[index].xpath(".//td[6]//span/text()").extract_first()
@@ -97,6 +108,9 @@ class Gys_Price_Spider(scrapy.Spider):
                 if "、" in pro_name:
                     category = pro_name.split("、")[1]
                     pro_name = pro_name.split("、")[1]
+                    if pro_name is "":
+                        category = "蓄禽产品"
+                        pro_name = "蓄禽产品"
 
                 # 处理原州区价格、隆德县价格、均价
                 yzq_price = all_tr[index].xpath(".//td[8]//span/text()").extract_first()
@@ -108,6 +122,20 @@ class Gys_Price_Spider(scrapy.Spider):
                 avg_price = all_tr[index].xpath(".//td[10]//span/text()").extract_first()
                 if avg_price is None:
                     avg_price = ""
-                print(pro_name + "," + category + "," + yzq_price + "," + ldx_price + "," + avg_price)
 
+                item['pro_name'] = pro_name
+                item['category'] = category
+                item['pro_unit'] = "元/公斤"
+                item['yzq_price'] = yzq_price.strip()
+                item['ldx_price'] = ldx_price.strip()
+                item['avg_price'] = avg_price.strip()
+                item['pub_time'] = response.meta["pub_time"]
+                item['price_from'] = "固原市农业农村局"
+
+                items.append(deepcopy(item))
+
+            if len(items) !=0 :
+                fp = open("./result.txt",'a+')
+                fp.write(str(items))
+                #print(items)
         pass
